8c5360d9d033150e6e52d16ebb058032b91eda00
renemel <rene.melendeza@gmail.com>
2019-11-10 09:58:35 -0600

    Informes

--- reserva-lab/app.js
+++ reserva-lab/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var logIn = require('./routes/login');
 var usersRouter = require('./routes/users');
+var informe = require('./routes/informe');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/login', logIn);
 app.use('/users', usersRouter);
+app.use('/informe', informe);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

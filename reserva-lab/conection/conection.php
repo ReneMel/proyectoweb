e6a855a277e778ce1cddc5d5b1cea67936c0f0d4
renemel <rene.melendeza@gmail.com>
2019-11-12 10:23:00 -0600

    try-catch

--- reserva-lab/conection/conection.php
+++ reserva-lab/conection/conection.php
@@ -1,3 +1,14 @@
 <?php
-    $dbconn2 = pg_connect("host=localhost port=5432 dbname=ucasoft user=renemel password=***..");
+    try{
+    
+        if ($dbconn2 = pg_connect("host=localhost port=5432 dbname=ucasoft user=renemel password=***..")){
+            echo "me gusta";
+        }else 
+        {
+            throw new Exception ('Unable to connect');
+        }
+    }
+    catch(Exception $e){
+        
+    }
 ?>

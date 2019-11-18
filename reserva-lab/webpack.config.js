cc7e08f76f1b3ecd5045147f57eb5d0de4dadba1
renemel <rene.melendeza@gmail.com>
2019-11-18 08:08:51 -0600

    [add] webpack

---
+++ reserva-lab/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './public/javascripts/calendar.js',
+  resolve: {
+    extensions: [ '.js' ]
+  },
+  output: {
+    filename: 'calendar.js',
+    path: path.join(__dirname, 'dist')
+  },
+  devtool: 'sourcemap'
+}

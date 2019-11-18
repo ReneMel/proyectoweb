a56eec106726e6441b5dfbec43081e12ecc4477d
renemel <rene.melendeza@gmail.com>
2019-11-17 21:32:38 -0600

    [Add]full-calendar funcional

--- reserva-lab/public/javascripts/calendar.js
+++ reserva-lab/public/javascripts/calendar.js
@@ -1,51 +1,12 @@
-$('#calendar').fullCalendar({
-    header: {
-      left: 'prev,next today',
-      center: 'addEventButton',
-      right: 'month,agendaWeek,agendaDay,listWeek'
-    },
-    defaultDate: '2018-11-16',
-    navLinks: true,
-    editable: true,
-    eventLimit: true,
-    events: [{
-        title: 'Simple static event',
-        start: '2018-11-16',
-        description: 'Super cool event'
-      },
-  
-    ],
-    customButtons: {
-      addEventButton: {
-        text: 'Add new event',
-        click: function () {
-          var dateStr = prompt('Enter date in YYYY-MM-DD format');
-          var date = moment(dateStr);
-  
-          if (date.isValid()) {
-            $('#calendar').fullCalendar('renderEvent', {
-              title: 'Dynamic event',
-              start: date,
-              allDay: true
-            });
-          } else {
-            alert('Invalid Date');
-          }
-  
-        }
-      }
-    },
-    dayClick: function (date, jsEvent, view) {
-      var date = moment(date);
-  
-      if (date.isValid()) {
-        $('#calendar').fullCalendar('renderEvent', {
-          title: 'Dynamic event from date click',
-          start: date,
-          allDay: true
-        });
-      } else {
-        alert('Invalid');
-      }
-    },
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin ]
   });
+
+  calendar.render();
+});

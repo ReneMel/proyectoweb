d8ae80ae6e1f14df1ecd212c5cda8b872818111e
renemel <rene.melendeza@gmail.com>
2019-11-22 11:46:20 -0600

    [test] droplist fill auto

--- reserva-lab/public/javascripts/addMateria.js
+++ reserva-lab/public/javascripts/addMateria.js
@@ -1,4 +1,5 @@
 let dataMaterias = document.getElementById('dataMaterias')
+let dropdown= document.getElementById('materias')
 
 
 document.addEventListener('DOMContentLoaded', async ()=>{
@@ -6,30 +7,48 @@ document.addEventListener('DOMContentLoaded', async ()=>{
     await fetch(`http://localhost:3000/matBU`)
     .then(res => {
           return res.json()
+          
     })
-    .then(data => {
-        let dropdown = $('#materias');
+    .then(data=>{
+        console.log(data[0].nombre)
+        let option;
+    
+    	for (let i = 0; i < data.length; i++) {
+          option = document.createElement('option');
+      	  option.text = data[i].nombre;
+      	  option.value = data[i].nombre;
+      	  dropdown.add(option);
+        }
+    })
+    /*.then(data => {
+        //let dropdown = $('#materias');
 
-        dropdown.empty();
+        //dropdown.empty();
         
-        dropdown.append('<option selected="true" disabled>Elija materia/Province</option>');
-        dropdown.prop('selectedIndex', 0);
+        //dropdown.append('<option selected="true" disabled>Elija materia</option>');
+        //dropdown.prop('selectedIndex', 0);
         
-        
-        data.map((res,index)=>{
-            console.log(res);
+        /*
+        data.map((object,index)=>{
             
+            $.each(object, function (key, entry) {
+                
+                
+                dropdown.append($('<option>').attr('value', entry.nombre).text(entry.nombre));
+                console.log(entry.nombre);
+                
+               //dropdown.append($("<option />").val(entry.nombre).text(entry.nombre));
+                
+               dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.name));
+
+
+              })
 
         // Populate dropdown with list of provinces
-        $.getJSON(res, function (data) {
-            console.log(data);
-            
-          $.each(data, function (key, entry) {
-              console.log(entry)
-            dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.nombre));
-          })
-        });
-        })
         
-    })
+        })
+
+
+        
+    })*/
 });
